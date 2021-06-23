ef2249b1a93057405659c9db7847030205891ae2
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2021-06-23 05:04:18 -0300

    Create Jugadores.h

---
+++ Jugadores.h
@@ -0,0 +1,26 @@
+#ifndef JUGADORES_H_
+#define JUGADORES_H_
+
+#include "Jugador.h"
+#include "Lista.h"
+
+class Jugadores {
+private:
+	Lista<Jugador*> *jugadores;
+	int idJugadorActual;
+	int cantidadJugadores;
+public:
+
+	Jugadores();
+
+	~Jugadores();
+
+	void agregarJugador(std::string ficha);
+
+	Jugador* getJugadorActual();
+
+	void pasarJugador();
+};
+
+
+#endif /* JUGADORES_H_ */
