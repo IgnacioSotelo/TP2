3c9357fc7f2604fa97ea43eef4cc919c52245c05
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2021-06-22 23:47:54 -0300

    Agrego pre y post condiciones

--- Columna.h
+++ Columna.h
@@ -1,10 +1,3 @@
-/*
- * Columna.h
- *
- *  Created on: 22 jun. 2021
- *      Author: ignacio
- */
-
 #ifndef COLUMNA_H_
 #define COLUMNA_H_
 #include "Lista.h"
@@ -13,18 +6,49 @@ class Columna {
 private:
 	Lista<int> *celdas;
 public:
+
+	/*
+	 * PRE : -
+	 * POST: Crea una nueva columna
+	 */
 	Columna();
 
+	/*
+	 * PRE : -
+	 * POST: Destructor
+	 */
 	~Columna();
 
+	/*
+	 * PRE : El dato ingresado sea de tipo 'int'
+	 * POST: Agrega una nueva celda a la columna
+	 */
 	void agregarCelda(int dato);
 
+	/*
+	 * PRE : El dato ingresado sea de tipo 'int'
+	 *       La posicion debe estar entre 1 y la cantidad de elementos en la columna
+	 * POST: Agrega una celda en una posicion especifica
+	 */
 	void agregarCelda(int dato, int posicion);
 
+	/*
+	 * PRE : El dato ingresado sea de tipo 'int'
+	 *       La posicion debe estar entre 1 y la cantidad de elementos en la columna
+	 * POST: Cambia el valor de una celda especifica
+	 */
 	void setCelda(int dato, int posicion);
 
+	/*
+	 * PRE : La posicion debe estar entre 1 y la cantidad de elementos en la columna
+	 * POST: Devuelve el valor de una celda
+	 */
 	int getCelda( int posicion);
 
+	/*
+	 * PRE : -
+	 * POST: Devuelve la cantidad de elementos en la columna
+	 */
 	int getTamanio();
 };
 
