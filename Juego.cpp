ddddd2002bf6a86f5321997fec26a68644652541
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2021-06-23 05:01:19 -0300

    Update Juego.cpp

--- Juego.cpp
+++ Juego.cpp
@@ -1,18 +1,22 @@
 #include "Juego.h"
 
+int Jugador::numero = 1;
+
 Juego::Juego(int filas, int columnas, int profundidad) {
 	tablero = new Tablero(filas, columnas, profundidad);
 	tablero->iniciarTablero();
+	jugadores = new Jugadores;
 }
 
 Juego::~Juego() {
 	delete tablero;
+	delete jugadores;
 }
 
-void Juego::colocarFicha(int ficha, int columna, int profundidad) {
+void Juego::colocarFicha(std::string ficha, int columna, int profundidad) {
 	int finDeColumna = tablero->getDimensionColumna();
 
-	while((tablero->getCelda(columna, finDeColumna, profundidad) != 0) && finDeColumna >= 1) {
+	while((tablero->getCelda(columna, finDeColumna, profundidad) != ".") && finDeColumna >= 1) {
 		finDeColumna--;
 	}
 
@@ -21,4 +25,16 @@ void Juego::colocarFicha(int ficha, int columna, int profundidad) {
 
 void Juego::mostrarTablero() {
 	tablero->mostrarTablero();
+}
+
+void Juego::agregarJugador(std::string ficha) {
+	jugadores->agregarJugador(ficha);
+}
+
+Jugador* Juego::getJugadorActual() {
+	return jugadores->getJugadorActual();
+}
+
+void Juego::pasarJugador() {
+	jugadores->pasarJugador();
 }
