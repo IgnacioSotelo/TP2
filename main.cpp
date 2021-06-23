2d0bc2dd20e52b997542f99b1fa814fa178331cd
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2021-06-23 04:59:36 -0300

    Update main

--- main.cpp
+++ main.cpp
@@ -3,17 +3,31 @@
 using namespace std;
 
 int main() {
-//	Con profundidad = 1 es una tabla 2d
-	Juego juego(4,4,1);
+	Juego juego(5,5,1);
 	int input = -1;
+	int contador = 1;
 
 	while(input != 0) {
+		string ficha;
+		cout << "Ingresar ficha para jugador " << contador++ << ": ";
+		cin >> ficha;
+		juego.agregarJugador(ficha);
+		cout << "Agregar otro jugador? 1: si  -  0: no ";
+		cin >> input;
+	}
+
+	input = -1;
+
+	while(input != 0) {
+		cout << "--------------------------------------------------------" << endl;
+		cout << "Turno de jugador: " << juego.getJugadorActual()->getNumero() << ", ficha " << juego.getJugadorActual()->getFicha()<< endl;
+
 		cout << "Ingresar columna: ";
 		cin >> input;
-		cout << endl;
 
-		juego.colocarFicha(1, input, 1);
+		juego.colocarFicha(juego.getJugadorActual()->getFicha(), input, 1);
 		juego.mostrarTablero();
+		juego.pasarJugador();
 	}
 
 	return 0;
