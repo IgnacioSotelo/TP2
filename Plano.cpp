5a705633818051ef37c645b7b95c2c7e0b7b846c
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2021-06-23 05:04:44 -0300

    Update Plano.cpp

--- Plano.cpp
+++ Plano.cpp
@@ -1,9 +1,3 @@
-/*
- * Plano.cpp
- *
- *  Created on: 22 jun. 2021
- *      Author: ignacio
- */
 #include "Plano.h"
 
 Plano::Plano() {
@@ -30,20 +24,18 @@ Columna* Plano::getColumna(int posicion) {
 	return columnas->obtener(posicion);
 }
 
-int Plano::getCelda( int x,  int y) {
+std::string Plano::getCelda( int x,  int y) {
 	return columnas->obtener(x)->getCelda(y);
 }
 
-void Plano::setCelda( int dato, int x,  int y) {
+void Plano::setCelda( std::string dato, int x,  int y) {
 	columnas->obtener(x)->setCelda(dato, y);
 }
 
 void Plano::imprimirPlano() {
 	for(int i = 1; i <= columnas->obtener(1)->getTamanio(); i++) {
 		for( int j = 1; j <= columnas->contarElementos(); j++) {
-			if(columnas->obtener(j)->getCelda(i) == 0) std::cout << " . ";
-			if(columnas->obtener(j)->getCelda(i) == 1) std::cout << " X ";
-			if(columnas->obtener(j)->getCelda(i) == 2) std::cout << " O ";
+			std::cout << " " << columnas->obtener(j)->getCelda(i) << " ";
 		}
 		std::cout << std::endl;
 	}
