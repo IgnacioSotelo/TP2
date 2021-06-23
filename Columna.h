d43cd051111fae2dd0e1985bfc9eb5276472933f
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2021-06-23 05:00:19 -0300

    Update Columna.h

--- Columna.h
+++ Columna.h
@@ -1,10 +1,11 @@
 #ifndef COLUMNA_H_
 #define COLUMNA_H_
 #include "Lista.h"
+#include <iostream>
 
 class Columna {
 private:
-	Lista<int> *celdas;
+	Lista<std::string> *celdas;
 public:
 
 	/*
@@ -23,27 +24,27 @@ public:
 	 * PRE : El dato ingresado sea de tipo 'int'
 	 * POST: Agrega una nueva celda a la columna
 	 */
-	void agregarCelda(int dato);
+	void agregarCelda(std::string dato);
 
 	/*
 	 * PRE : El dato ingresado sea de tipo 'int'
 	 *       La posicion debe estar entre 1 y la cantidad de elementos en la columna
 	 * POST: Agrega una celda en una posicion especifica
 	 */
-	void agregarCelda(int dato, int posicion);
+	void agregarCelda(std::string dato, int posicion);
 
 	/*
 	 * PRE : El dato ingresado sea de tipo 'int'
 	 *       La posicion debe estar entre 1 y la cantidad de elementos en la columna
 	 * POST: Cambia el valor de una celda especifica
 	 */
-	void setCelda(int dato, int posicion);
+	void setCelda(std::string dato, int posicion);
 
 	/*
 	 * PRE : La posicion debe estar entre 1 y la cantidad de elementos en la columna
 	 * POST: Devuelve el valor de una celda
 	 */
-	int getCelda( int posicion);
+	std::string getCelda( int posicion);
 
 	/*
 	 * PRE : -
