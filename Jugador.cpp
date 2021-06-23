3c67a8a80981c43a2418395f6fc43c358fb405fd
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2021-06-23 17:47:16 -0300

    Update Jugador.cpp

--- Jugador.cpp
+++ Jugador.cpp
@@ -3,9 +3,9 @@
 Jugador::Jugador(std::string ficha) {
 	id = numero;
 	numero++;
-	cartaBloquearTurno = true;
-	cartaJuegaDoble = true;
-	cartaDobleFicha = true;
+	cartaBloquearTurno = false;
+	cartaJuegaDoble = false;
+	cartaDobleFicha = false;
 	fichasRestantes = 50;
 	cartasRestantes = 3;
 	this->ficha = ficha;
@@ -18,3 +18,40 @@ int Jugador::getNumero() {
 std::string Jugador::getFicha() {
 	return ficha;
 }
+
+int Jugador::getCartasRestantes() {
+	return cartasRestantes;
+}
+
+int Jugador::getFichasRestantes() {
+	return fichasRestantes;
+}
+
+bool Jugador::getCartaBloquearTurno() {
+	return cartaBloquearTurno;
+}
+
+bool Jugador::getCartaJuegaDoble() {
+	return cartaJuegaDoble;
+}
+
+bool Jugador::getCartaDobleFicha() {
+	return cartaDobleFicha;
+}
+
+void Jugador::usarCartaBloquearTurno() {
+	cartaBloquearTurno = true;
+}
+
+void Jugador::usarCartaJuegaDoble() {
+	cartaJuegaDoble = true;
+}
+
+void Jugador::usarCartaDobleFicha() {
+	cartaDobleFicha = true;
+}
+
+void Jugador::restarCarta() {
+	cartasRestantes--;
+}
+
