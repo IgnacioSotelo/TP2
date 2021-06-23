ca031aee9e9278a5290ea1d400dd6d5ffb05c7c7
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2021-06-22 23:49:28 -0300

    Agrego pre y post condiciones

--- Espacio.h
+++ Espacio.h
@@ -1,10 +1,3 @@
-/*
- * Espacio.h
- *
- *  Created on: 22 jun. 2021
- *      Author: ignacio
- */
-
 #ifndef ESPACIO_H_
 #define ESPACIO_H_
 
@@ -14,22 +7,60 @@ class Espacio {
 private:
 	Lista<Plano*>* planos;
 public:
+	/*
+	 * PRE : -
+	 * POST: Crea un nuevo espacio.
+	 *       Un espacio es un conjunto de planos
+	 */
 	Espacio();
 
+	/*
+	 * PRE : -
+	 * POST: Destructor
+	 */
 	~Espacio();
 
+	/*
+	 * PRE : -
+	 * POST: Agrega un plano (conjunto de columnas) al espacio
+	 */
 	void agregarPlano(Plano* plano);
 
+	/*
+	 * PRE : -
+	 * POST: Devuelve la cantidad de planos en el espacio
+	 */
 	int getTamanio();
 
+	/*
+	 * PRE : Las columnas deben tener todas la misma dimension
+	 * POST: Devuelve la cantidad de elementos que tienen las columnas
+	 */
 	int getDimensionColumna();
 
+	/* 
+	 * PRE : -
+	 * POST: Devuelve un plano a partir de una posicion
+	 */
 	Plano* getPlano(int posicion);
 
+	/*
+	 * PRE : Introducir una coordenada valida
+	 * POST: Devuelve una celda en una coordenada dada (x,y,z)
+	 */
 	int getCelda( int x,  int y, int z);
 
+	/*
+	 * PRE : La celda debe existir
+	 * POST: Cambia el valor de una celda dando sus coordenadas (x,y,z)
+	 */
 	void setCelda(int dato, int x, int y, int z);
 
+	/* NOTA: Para pruebas nada mas, tratar de que la impresion sea desde otro objeto..
+	 * PRE : -
+	 * POST: Imprime en pantalla el espacio siendo los elemntos mas profundos
+	 *       los ultimos en imprimirse (quizas sea mejor que sea al reves)
+	 */
 	void imprimirEspacio();
 };
 
