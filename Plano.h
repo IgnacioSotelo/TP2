8a778e805ee39d9f6849a82a71a0665c3fbb9abb
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2021-06-22 23:50:35 -0300

    Agrego pre y post condiciones

--- Plano.h
+++ Plano.h
@@ -1,10 +1,3 @@
-/*
- * Plano.h
- *
- *  Created on: 22 jun. 2021
- *      Author: ignacio
- */
-
 #ifndef PLANO_H_
 #define PLANO_H_
 
@@ -16,22 +9,60 @@ class Plano {
 private:
 	Lista<Columna*>* columnas;
 public:
+
+	/*
+	 * PRE : -
+	 * POST: Crea un nuevo plano
+	 *       Un plano es un conjunto de columnas
+	 */
 	Plano();
 
+	/*
+	 * PRE : -
+	 * POST: Destructor
+	 */
 	~Plano();
 
+	/*
+	 * PRE : -
+	 * POST: Agrega una columna al plano
+	 */
 	void agregarColumna(Columna *columna);
 
+	/*
+	 * PRE : -
+	 * POST: Devuelva la cantidad de columnas dentro del plano
+	 */
 	int getTamanio();
 
+	/*
+	 * PRE : Todas las columnas deben tener la misma dimension
+	 * POST: Devuelve la cantidad de elementos que tienen las columnas
+	 */
 	int getDimensionColumna();
 
+	/*
+	 * PRE : -
+	 * POST: Devuelve una columna en particular
+	 */
 	Columna *getColumna(int posicion);
 
+	/*
+	 * PRE : -
+	 * POST: Devuelve el valor de una celda dentro del plano
+	 */
 	int getCelda( int x,  int y);
 
+	/*
+	 * PRE : La celda debe existir
+	 * POST: Cambia el valor de una celda en particular
+	 */
 	void setCelda( int dato, int x,  int y);
 
+	/* Para pruebas, buscar otra alternativa quizas sea mejor..
+	 * PRE : -
+	 * POST: Imprime en pantalla el plano completo
+	 */
 	void imprimirPlano();
 };
 
